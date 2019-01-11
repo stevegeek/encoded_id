0e853edf03ef3a3060549848dd8f6790611bf068
Stephen Ierodiaconou <stevegeek@gmail.com>
2019-01-11 17:57:14 +0100

    Change WithUid to allow ID constraint on UID find, and add version of method that raises exceptions

--- app/core/with_uid.rb
+++ app/core/with_uid.rb
@@ -5,8 +5,23 @@ module Core
     extend ActiveSupport::Concern
 
     module ClassMethods
-      def find_by_uid(uid)
-        find_by(id: Core::ReversableId.decode(uid))
+      # Find by UID and optionally ensure record ID is the same as constraint
+      def find_by_uid(uid, with_id: nil)
+        record = find_by(id: Core::ReversableId.decode(uid))
+        return unless record
+        unless with_id.nil?
+          return unless with_id == record.id
+        end
+        record
+      end
+
+      def find_by_uid!(uid, with_id: nil)
+        record = find_by(id: Core::ReversableId.decode(uid))
+        raise ActiveRecord::RecordNotFound unless record
+        unless with_id.nil?
+          raise ActiveRecord::RecordNotFound unless with_id == record.id
+        end
+        record
       end
     end
 
