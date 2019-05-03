08cd1bf292a22f18749e747d15fd4b9203b60fc5
Stephen Ierodiaconou <stevegeek@gmail.com>
2019-05-03 17:20:47 +0100

    Add more methods to Core::WithUid to create other IDs with name in etc, for more human friensly urls

--- app/core/with_uid.rb
+++ app/core/with_uid.rb
@@ -4,30 +4,110 @@ module Core
   module WithUid
     extend ActiveSupport::Concern
 
-    module ClassMethods
+    class_methods do
       # Find by UID and optionally ensure record ID is the same as constraint
       def find_by_uid(uid, with_id: nil)
-        record = find_by(id: Core::ReversableId.decode(uid))
-        return unless record
-        unless with_id.nil?
-          return unless with_id == record.id
-        end
-        record
+        find_via_custom_id(Core::ReversableId.decode(uid), :id, compare_to: with_id)
       end
 
       def find_by_uid!(uid, with_id: nil)
-        record = find_by(id: Core::ReversableId.decode(uid))
-        raise ActiveRecord::RecordNotFound unless record
+        find_via_custom_id!(Core::ReversableId.decode(uid), :id, compare_to: with_id)
+      end
+
+      def find_by_fixed_slug(slug, attribute: :slug, with_id: nil)
+        find_via_custom_id(slug, attribute, compare_to: with_id)
+      end
+
+      def find_by_fixed_slug!(slug, attribute: :slug, with_id: nil)
+        find_via_custom_id!(slug, attribute, compare_to: with_id)
+      end
+
+      def find_by_slugged_uid(slugged_uid, with_id: nil)
+        uid = get_id_from_slugged(slugged_uid)
+        find_by_uid(uid, with_id: with_id)
+      end
+
+      def find_by_slugged_uid!(slugged_uid, with_id: nil)
+        uid = get_id_from_slugged(slugged_uid)
+        find_by_uid!(uid, with_id: with_id)
+      end
+
+      def find_by_slugged_id(slugged_id, with_id: nil)
+        id_part = get_ids_from_slugged(slugged_id)
         unless with_id.nil?
-          raise ActiveRecord::RecordNotFound unless with_id == record.id
+          return unless with_id == id_part
         end
-        record
+        find(id_part)
+      end
+
+      def find_by_slugged_id!(slugged_id, with_id: nil)
+        id_part = get_ids_from_slugged(slugged_id)
+        unless with_id.nil?
+          raise ActiveRecord::RecordNotFound unless with_id == id_part
+        end
+        find!(id_part)
       end
     end
 
+    # Instance methods
+
     def uid
-      return unless id
+      raise(StandardError, "The model has no ID") unless id
       Core::ReversableId.encode(id)
+    end
+
+    # (slug)--(hash id)
+    def slugged_uid(with: :name)
+      generate_composite_id(with, :uid)
+    end
+
+    # (name slug)-(record id)
+    def slugged_id(with: :name)
+      generate_composite_id(with, :id)
+    end
+
+    # Private class methods
+
+    included do
+      class << self
+        private
+
+        def get_id_from_slugged(slugged)
+          slugged.split("--").last
+        end
+
+        def get_ids_from_slugged(slugged)
+          get_id_from_slugged(slugged).split("-")
+        end
+
+        def find_via_custom_id(value, attribute, compare_to: nil)
+          record = find_by(Hash[attribute, value])
+          return unless record
+          unless compare_to.nil?
+            return unless compare_to == record.send(attribute)
+          end
+          record
+        end
+
+        def find_via_custom_id!(value, attribute, compare_to: nil)
+          record = find_by!(Hash[attribute, value])
+          unless compare_to.nil?
+            raise ActiveRecord::RecordNotFound unless compare_to == record.send(attribute)
+          end
+          record
+        end
+      end
+    end
+
+    private
+
+    def generate_composite_id(name_method, id_method)
+      name_part = send(name_method)
+      id_part = send(id_method)
+      unless id_part.present? && name_part.present?
+        raise(StandardError, "The model has no #{id_method} or #{name_method}")
+      end
+      "#{name_part.to_s.parameterize}--#{id_part}"
     end
   end
 end
---
+++ spec/core/with_uid_spec.rb
@@ -0,0 +1,190 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Core::WithUid do
+  describe "boolean attributes" do
+    class TestWithUidClass
+      include Core::TypedAttributesModel
+      include Core::WithUid
+
+      attr_string :id
+      attr_string :name, default: "My Favourite Shop!"
+      attr_string :title, default: "Beef Tenderloins/Prime"
+
+      def self.find_by(_id); end
+
+      def self.find_by!(_id); end
+
+      def self.find(_id); end
+
+      def self.find!(_id); end
+    end
+
+    subject(:test_instance) { TestWithUidClass.new(id: "123") }
+
+    let(:test_record) { TestWithUidClass.new(id: "123", name: "Foo", title: "Bar") }
+
+    before do
+      allow(TestWithUidClass).to receive(:find_by).with(id: [123]) { test_record }
+      allow(TestWithUidClass).to receive(:find_by).with(id: [124]).and_return(nil)
+      allow(TestWithUidClass).to receive(:find_by!).with(id: [123]) { test_record }
+      allow(TestWithUidClass).to receive(:find_by!).with(id: [124]).and_raise(ActiveRecord::RecordNotFound)
+    end
+
+    describe "#uid" do
+      it "generates a reversible ID" do
+        expect(test_instance.uid).to eql "jex8-bgwn-egyr-ad9z"
+      end
+    end
+
+    describe "#slugged_uid" do
+      it "generates a slugged ID reversible with default attribute" do
+        expect(test_instance.slugged_uid).to eql "my-favourite-shop--jex8-bgwn-egyr-ad9z"
+      end
+
+      it "generates a slugged ID reversible with specific attribute" do
+        expect(test_instance.slugged_uid(with: :title)).to eql "beef-tenderloins-prime--jex8-bgwn-egyr-ad9z"
+      end
+    end
+
+    describe "#slugged_id" do
+      it "generates a slugged ID with default attribute" do
+        expect(test_instance.slugged_id).to eql "my-favourite-shop--123"
+      end
+
+      it "generates a slugged ID with specific attribute" do
+        expect(test_instance.slugged_id(with: :title)).to eql "beef-tenderloins-prime--123"
+      end
+    end
+
+    describe ".find_by_uid" do
+      it "returns the record" do
+        expect(TestWithUidClass.find_by_uid("jex8-bgwn-egyr-ad9z")).to eql test_record
+      end
+
+      it "returns the record if the constraint id is the same" do
+        expect(TestWithUidClass.find_by_uid("jex8-bgwn-egyr-ad9z", with_id: "123")).to eql test_record
+      end
+
+      it "returns nil if the constraint id is not the same" do
+        expect(TestWithUidClass.find_by_uid("jex8-bgwn-egyr-ad9z", with_id: "456")).to be nil
+      end
+
+      it "returns nil if the record does not exist" do
+        expect(TestWithUidClass.find_by_uid("bpng-e8yb-emwd-kmx9", with_id: "456")).to be nil
+      end
+    end
+
+    describe ".find_by_uid!" do
+      it "returns the record" do
+        expect(TestWithUidClass.find_by_uid!("jex8-bgwn-egyr-ad9z")).to eql test_record
+      end
+
+      it "returns the record if the constraint id is the same" do
+        expect(TestWithUidClass.find_by_uid!("jex8-bgwn-egyr-ad9z", with_id: "123")).to eql test_record
+      end
+
+      it "raises if the constraint id is not the same" do
+        expect {
+          TestWithUidClass.find_by_uid!("jex8-bgwn-egyr-ad9z", with_id: "456")
+        }.to raise_error ActiveRecord::RecordNotFound
+      end
+
+      it "raises if the record does not exist" do
+        expect {
+          TestWithUidClass.find_by_uid!("bpng-e8yb-emwd-kmx9", with_id: "456")
+        }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+
+    context "with fixed DB slugs" do
+      before do
+        allow(TestWithUidClass).to receive(:find_by).with(slug: "slugA") { test_record }
+        allow(TestWithUidClass).to receive(:find_by).with(slug: "slugB").and_return(nil)
+        allow(TestWithUidClass).to receive(:find_by!).with(slug: "slugA") { test_record }
+        allow(TestWithUidClass).to receive(:find_by!).with(slug: "slugB").and_raise(ActiveRecord::RecordNotFound)
+      end
+
+      describe ".find_by_fixed_slug" do
+        it "returns the record" do
+          expect(TestWithUidClass.find_by_fixed_slug("slugA")).to eql test_record
+        end
+
+        it "returns nil if the record does not exist" do
+          expect(TestWithUidClass.find_by_fixed_slug("slugB")).to be nil
+        end
+      end
+
+      describe ".find_by_fixed_slug!" do
+        it "returns the record" do
+          expect(TestWithUidClass.find_by_fixed_slug!("slugA")).to eql test_record
+        end
+
+        it "returns nil if the record does not exist" do
+          expect {
+            TestWithUidClass.find_by_fixed_slug!("slugB")
+          }.to raise_error ActiveRecord::RecordNotFound
+        end
+      end
+    end
+
+    describe ".find_by_slugged_uid" do
+      it "returns the record" do
+        expect(TestWithUidClass.find_by_slugged_uid("slug--jex8-bgwn-egyr-ad9z")).to eql test_record
+      end
+
+      it "returns nil if the record does not exist" do
+        expect(TestWithUidClass.find_by_slugged_uid("slug--bpng-e8yb-emwd-kmx9")).to be nil
+      end
+    end
+
+    describe ".find_by_slugged_uid!" do
+      it "returns the record" do
+        expect(TestWithUidClass.find_by_slugged_uid!("slug--jex8-bgwn-egyr-ad9z")).to eql test_record
+      end
+
+      it "returns nil if the record does not exist" do
+        expect {
+          TestWithUidClass.find_by_slugged_uid!("slug--bpng-e8yb-emwd-kmx9")
+        }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+
+    context "when finding by id" do
+      before do
+        allow(TestWithUidClass).to receive(:find).with(["123"]) { test_record }
+        allow(TestWithUidClass).to receive(:find).with(%w[123 234]) { test_record }
+        allow(TestWithUidClass).to receive(:find).with(["456"]).and_return(nil)
+        allow(TestWithUidClass).to receive(:find!).with(["123"]) { test_record }
+        allow(TestWithUidClass).to receive(:find!).with(["456"]).and_raise(ActiveRecord::RecordNotFound)
+      end
+
+      describe ".find_by_slugged_id" do
+        it "returns the record" do
+          expect(TestWithUidClass.find_by_slugged_id("slug--123")).to eql test_record
+        end
+
+        it "returns the records with multiple ids" do
+          expect(TestWithUidClass.find_by_slugged_id("slug--123-234")).to eql test_record
+        end
+
+        it "returns nil if the record does not exist" do
+          expect(TestWithUidClass.find_by_slugged_id("slug--456")).to be nil
+        end
+      end
+
+      describe ".find_by_slugged_id!" do
+        it "returns the record" do
+          expect(TestWithUidClass.find_by_slugged_id!("slug--123")).to eql test_record
+        end
+
+        it "returns nil if the record does not exist" do
+          expect {
+            TestWithUidClass.find_by_slugged_id!("slug--456")
+          }.to raise_error ActiveRecord::RecordNotFound
+        end
+      end
+    end
+  end
+end
