d274de6b6513a0641d4bd433df06536e3decaa93
Stephen Ierodiaconou <stevegeek@gmail.com>
2023-08-06 17:33:57 +0200

    Refactor the alphabet class

--- lib/encoded_id/alphabet.rb
+++ lib/encoded_id/alphabet.rb
@@ -2,29 +2,10 @@
 
 module EncodedId
   class Alphabet
-    def initialize(characters, equivalences = nil)
-      unless (characters.is_a?(Array) || characters.is_a?(String)) && characters.size > 0
-        raise InvalidAlphabetError, "Alphabet must be a string or array"
-      end
-      unique_alphabet = (characters.is_a?(Array) ? characters : characters.chars).uniq
-      raise InvalidAlphabetError, "Alphabet must be at least 16 unique characters" if unique_alphabet.size < 16
-      @characters = unique_alphabet.join
-
-      # Equivalences is a hash of characters to their equivalent character.
-      # Characters to be mapped must not be in the alphabet, and must map to a character that is in the alphabet.
-      raise InvalidConfigurationError, "Character equivalences must be a hash or nil" unless equivalences.nil? || equivalences.is_a?(Hash)
-      valid_equivalences = equivalences.nil? || (unique_alphabet & equivalences.keys).empty? && (equivalences.values - unique_alphabet).empty?
-      raise InvalidConfigurationError unless valid_equivalences
-      @equivalences = equivalences
-    end
-
-    attr_reader :characters, :equivalences
+    MIN_UNIQUE_CHARACTERS = 16
 
     class << self
       def modified_crockford
-        # Note we downcase first, so mappings are only for lower case. Also Crockford suggests i==1,
-        # but here i==j is used.
-
         new(
           "0123456789abcdefghjkmnpqrstuvwxyz",
           {
@@ -34,6 +15,51 @@ module EncodedId
           }
         )
       end
+    end
+
+    def initialize(characters, equivalences = nil)
+      raise_invalid_alphabet! unless valid_input_characters?(characters)
+      unique_characters = unique_character_alphabet(characters)
+      raise_character_set_too_small! unless sufficient_characters?(unique_characters)
+      raise_invalid_equivalences! unless valid_equivalences?(equivalences, unique_characters)
+
+      @characters = unique_characters.join
+      @equivalences = equivalences
+    end
+
+    attr_reader :characters, :equivalences
+
+    private
+
+    def valid_input_characters?(characters)
+      (characters.is_a?(Array) || characters.is_a?(String)) && characters.size > 0
+    end
+
+    def unique_character_alphabet(characters)
+      (characters.is_a?(Array) ? characters : characters.chars).uniq
+    end
+
+    def sufficient_characters?(unique_alphabet)
+      unique_alphabet.size >= MIN_UNIQUE_CHARACTERS
+    end
+
+    def valid_equivalences?(equivalences, unique_characters)
+      return true if equivalences.nil?
+      return false unless equivalences.is_a?(Hash)
+
+      (unique_characters & equivalences.keys).empty? && (equivalences.values - unique_characters).empty?
+    end
+
+    def raise_invalid_alphabet!
+      raise InvalidAlphabetError, "Alphabet must be a string or array."
+    end
+
+    def raise_character_set_too_small!
+      raise InvalidAlphabetError, "Alphabet must contain at least #{MIN_UNIQUE_CHARACTERS} unique characters."
+    end
+
+    def raise_invalid_equivalences!
+      raise InvalidConfigurationError, "Character equivalences must be a hash or nil."
     end
   end
 end
---
+++ test/encoded_id/alphabet_test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestAlphabet < Minitest::Test
+  def test_initialize_with_valid_alphabet_string
+    alphabet = EncodedId::Alphabet.new("abcdefghijklmnopqrstuvwxyz0123456789")
+    assert_equal "abcdefghijklmnopqrstuvwxyz0123456789", alphabet.characters
+    assert_nil alphabet.equivalences
+  end
+
+  def test_initialize_with_valid_alphabet_array
+    alphabet = EncodedId::Alphabet.new(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+    assert_equal "abcdefghijklmnopqrstuvwxyz0123456789", alphabet.characters
+    assert_nil alphabet.equivalences
+  end
+
+  def test_initialize_with_invalid_alphabet
+    assert_raises EncodedId::InvalidAlphabetError do
+      EncodedId::Alphabet.new("abc")
+    end
+  end
+
+  def test_initialize_with_valid_equivalences
+    alphabet = EncodedId::Alphabet.new("abcdefghijklmnopqrstuvwxyz0123456789", {"*" => "a"})
+    assert_equal "abcdefghijklmnopqrstuvwxyz0123456789", alphabet.characters
+    assert_equal({"*" => "a"}, alphabet.equivalences)
+  end
+
+  def test_initialize_with_invalid_equivalences
+    assert_raises EncodedId::InvalidConfigurationError do
+      EncodedId::Alphabet.new("abcdefghijklmnopqrstuvwxyz0123456789", {"a" => "*"})
+    end
+  end
+
+  def test_modified_crockford
+    alphabet = EncodedId::Alphabet.modified_crockford
+    assert_equal "0123456789abcdefghjkmnpqrstuvwxyz", alphabet.characters
+    assert_equal({"o" => "0", "i" => "j", "l" => "1"}, alphabet.equivalences)
+  end
+end
