3ff51cdf1c6353fa21fe9afbeef5ec1117f67022
Stephen Ierodiaconou <stevegeek@gmail.com>
2025-04-22 13:47:47 +0200

    Add a Railtie for encoded_id-rails

--- lib/encoded_id/rails.rb
+++ lib/encoded_id/rails.rb
@@ -14,6 +14,7 @@ require_relative "rails/path_param"
 require_relative "rails/slugged_path_param"
 require_relative "rails/model"
 require_relative "rails/persists"
+require_relative "rails/railtie"
 
 module EncodedId
   module Rails
---
+++ lib/encoded_id/rails/railtie.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module EncodedId
+  module Rails
+    # Railtie for Rails integration
+    class Railtie < ::Rails::Railtie
+      # initializer "encoded_id.rails.initialize" do
+      # end
+    end
+  end
+end
