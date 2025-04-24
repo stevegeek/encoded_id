4e38efad2db9e1336091b79a6d4ef0360fb61687
Stephen Ierodiaconou <stevegeek@gmail.com>
2025-04-24 13:53:26 +0200

    Sqids takes a single hash argument

--- lib/encoded_id/encoders/sqids.rb
+++ lib/encoded_id/encoders/sqids.rb
@@ -6,9 +6,11 @@ module EncodedId
       def initialize(salt, min_hash_length = 0, alphabet = Alphabet.alphanum, blocklist = nil, my_sqids = nil)
         super
         @sqids = (my_sqids ? MySqids : ::Sqids).new(
-          min_length: min_hash_length,
-          alphabet: alphabet.characters,
-          blocklist: blocklist
+          {
+            min_length: min_hash_length,
+            alphabet: alphabet.characters,
+            blocklist: blocklist
+          }
         )
       rescue TypeError, ArgumentError => e
         raise InvalidInputError, "unable to create sqids instance: #{e.message}"
